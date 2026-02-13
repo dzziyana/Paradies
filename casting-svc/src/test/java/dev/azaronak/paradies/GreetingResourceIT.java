52f4c45fac2ec6ed9a4fb945524a440b2447645e
Simon Scatton <simon.scatton@outlook.fr>
2026-02-13 18:08:42 +0100

    chore(casting-svc): created casting service quarkus app

---
+++ casting-svc/src/test/java/dev/azaronak/paradies/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package dev.azaronak.paradies;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
